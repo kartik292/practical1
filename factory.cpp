7b7b71811d2171421547df80ce2ee3da79157518
kartik292 <119070097+kartik292@users.noreply.github.com>
2023-11-25 15:36:56 +0530

    Add files via upload

---
+++ factory.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+class animal{
+    public:
+    virtual void speak()=0;
+
+    static animal *createanimal(int type);
+};
+
+class dog: public animal{
+    public:
+    void speak(){
+        cout<<"Woof Woof"<<endl;
+    }
+};
+
+class cat:public animal{
+    public:
+
+    void speak(){
+        cout<<"Meow Meow"<<endl;
+    }
+};
+
+animal *animal::createanimal(int type){
+    if(type==1){
+        return new dog();
+    }
+    else if (type==2){
+        return new cat();
+    }
+    else{
+        return nullptr;
+    }
+}
+
+int main(){
+    animal *dog1=animal::createanimal(1);
+    dog1->speak();
+    animal *cat1=animal::createanimal(2);
+    cat1->speak();
+    return 0;
+}
